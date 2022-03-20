7affd5047c519fae5a2db37ca367d09e657cd480
syaiful919 <syaiful919@gmail.com>
2022-03-20 22:05:44 +0700

    add some function to modify data

--- lib/ui/state/quiz_state_widget.dart
+++ lib/ui/state/quiz_state_widget.dart
@@ -1,7 +1,3 @@
-// TODO: 4. Host the data using the QuizStateScope
-
-// TODO: 5. Provide APIs to modify the data
-
 import 'package:benar_salah/core/service/quiz_service.dart';
 import 'package:benar_salah/ui/state/quiz_state.dart';
 import 'package:benar_salah/ui/state/quiz_state_scope.dart';
@@ -32,6 +28,71 @@ class _QuizStateWidgetState extends State<QuizStateWidget> {
     answerMap: {},
     currentIndex: 0,
   );
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+  }
+
+  void _initData() {
+    final allQuiz = widget.service.getQuiz();
+
+    setState(() {
+      _data = _data.copyWith(allQuiz: allQuiz);
+    });
+
+    _randomizeQuiz();
+  }
+
+  Future<void> _randomizeQuiz() async {
+    final allQuiz = _data.allQuiz;
+    allQuiz.shuffle();
+
+    final selectedQuiz = allQuiz.take(5).toList();
+
+    setState(() {
+      _data = _data.copyWith(selectedQuiz: selectedQuiz);
+    });
+  }
+
+  void selectAnswer(bool isCorrect) {
+    final newAnswerMap = {
+      ..._data.answerMap,
+      _data.currentIndex: isCorrect,
+    };
+
+    final isLastQuestion = _data.currentIndex == _data.selectedQuiz.length - 1;
+    final nextIndex = _data.currentIndex + 1;
+
+    setState(() {
+      _data = _data.copyWith(
+        currentIndex: isLastQuestion ? _data.currentIndex : nextIndex,
+        answerMap: newAnswerMap,
+      );
+    });
+  }
+
+  void resetQuiz() {
+    setState(() {
+      _data = _data.copyWith(
+        selectedQuiz: [],
+        currentIndex: 0,
+        answerMap: {},
+      );
+    });
+
+    _randomizeQuiz();
+  }
+
+  void resetAnswer() {
+    setState(() {
+      _data = _data.copyWith(
+        currentIndex: 0,
+        answerMap: {},
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
