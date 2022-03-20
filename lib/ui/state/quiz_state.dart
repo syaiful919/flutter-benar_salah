bede1b87221df3ee1179bf1f495e6b4b8a233f1f
syaiful919 <syaiful919@gmail.com>
2022-03-20 21:59:42 +0700

    create QuizState

--- lib/ui/state/quiz_state.dart
+++ lib/ui/state/quiz_state.dart
@@ -1,1 +1,29 @@
-// TODO: 1. Create QuizState
+import 'package:benar_salah/core/model/quiz.dart';
+
+class QuizState {
+  final List<Quiz> selectedQuiz;
+  final List<Quiz> allQuiz;
+  final Map<int, bool> answerMap;
+  final int currentIndex;
+
+  QuizState({
+    required this.allQuiz,
+    required this.selectedQuiz,
+    required this.answerMap,
+    required this.currentIndex,
+  });
+
+  QuizState copyWith({
+    List<Quiz>? allQuiz,
+    List<Quiz>? selectedQuiz,
+    Map<int, bool>? answerMap,
+    int? currentIndex,
+  }) {
+    return QuizState(
+      allQuiz: allQuiz ?? this.allQuiz,
+      selectedQuiz: selectedQuiz ?? this.selectedQuiz,
+      answerMap: answerMap ?? this.answerMap,
+      currentIndex: currentIndex ?? this.currentIndex,
+    );
+  }
+}
