39094f70e64089790bbe277cf0b0841355fef570
syaiful919 <syaiful919@gmail.com>
2022-03-20 22:07:01 +0700

    wrap MaterialApp with QuizStateWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:benar_salah/core/service/quiz_service.dart';
 import 'package:benar_salah/ui/pages/home/home_page.dart';
+import 'package:benar_salah/ui/state/quiz_state_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +12,13 @@ class QuizApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: 6. Insert QuizStateWidget into the widget tree
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Benar atau Salah',
-      home: HomePage(),
+    return const QuizStateWidget(
+      service: QuizService(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Benar atau Salah',
+        home: HomePage(),
+      ),
     );
   }
 }
