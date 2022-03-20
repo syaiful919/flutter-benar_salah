0669fdec88d45b1ca92079f0927457be2349f81e
syaiful919 <syaiful919@gmail.com>
2022-03-20 22:00:20 +0700

    create QuizStateScope

--- lib/ui/state/quiz_state_scope.dart
+++ lib/ui/state/quiz_state_scope.dart
@@ -1,1 +1,21 @@
-// TODO: 2. Create QuizStateScope which extends InheritedWidget
+import 'package:benar_salah/ui/state/quiz_state.dart';
+import 'package:flutter/material.dart';
+
+class QuizStateScope extends InheritedWidget {
+  const QuizStateScope({
+    Key? key,
+    required this.data,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final QuizState data;
+
+  static QuizState of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<QuizStateScope>()!.data;
+  }
+
+  @override
+  bool updateShouldNotify(QuizStateScope oldWidget) {
+    return data != oldWidget.data;
+  }
+}
