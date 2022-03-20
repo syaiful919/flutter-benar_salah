b7c4984f1e4268c16db8f6624248110f14ac5a3c
syaiful919 <syaiful919@gmail.com>
2022-03-20 22:02:23 +0700

    create QuizStateWidget

--- lib/ui/state/quiz_state_widget.dart
+++ lib/ui/state/quiz_state_widget.dart
@@ -1,5 +1,31 @@
-// TODO: 3. Create QuizStateWidget which extends StatefulWidget 
+// TODO: 4. Host the data using the QuizStateScope
 
-// TODO: 4. Host the data using the QuizStateScope 
+// TODO: 5. Provide APIs to modify the data
 
-// TODO: 5. Provide APIs to modify the data 
+import 'package:benar_salah/core/service/quiz_service.dart';
+import 'package:flutter/material.dart';
+
+class QuizStateWidget extends StatefulWidget {
+  final QuizService service;
+  final Widget child;
+
+  const QuizStateWidget({
+    Key? key,
+    required this.service,
+    required this.child,
+  }) : super(key: key);
+
+  static _QuizStateWidgetState of(BuildContext context) {
+    return context.findAncestorStateOfType<_QuizStateWidgetState>()!;
+  }
+
+  @override
+  State<QuizStateWidget> createState() => _QuizStateWidgetState();
+}
+
+class _QuizStateWidgetState extends State<QuizStateWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
